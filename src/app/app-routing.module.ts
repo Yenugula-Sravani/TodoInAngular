84a090afae12affc91c97fb0f5da033ed2acb3c3
Yenugula-sravani <yenugula.sravani@kumaran.com>
2023-10-19 16:07:07 +0530

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { FormComponent } from './components/form/form.component';
 
 
 const routes: Routes = [
-  
+  {path:"",component:WelcomeComponent},
+  {path:"form",component:FormComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { FormsModule } from '@angular/forms';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
